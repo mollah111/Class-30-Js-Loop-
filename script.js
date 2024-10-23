46547b94d8814ad12434ef93d6fa10a6d0fc6612
mollah111 <jalammollah111@gmail.com>
2024-10-23 21:25:06 +0600

    Js loop

---
+++ script.js
@@ -0,0 +1,22 @@
+
+// for(let i=1; i<=10; i++){
+//     console.log("Hellow World");
+//     console.log(i);
+// }
+
+// let marks = [40, 57, 68, 77, 87, 96];
+
+// for(let i=0; i<=5; i++){
+//     console.log(marks[i]);
+// }
+
+let numbers = [38, 45, 56, 69, 84, 99, 120, 135, 148, 160]
+
+for(let i=0; i<=9; i++){
+    if(numbers[i]%2 == 0){
+        console.log("Even Number=" + numbers[i]);
+    }
+    else{
+        console.log("Odd Number=" + numbers[i]);
+    }
+}
